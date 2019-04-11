09114a5dcd916175644ed3f9d4743d3d2e0a0fe0
chelseashin <chaewonshin95@gmail.com>
2019-04-11 04:40:34 +0000

    10_Customize profile

--- boards/views.py
+++ boards/views.py
@@ -1,11 +1,12 @@
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404, get_list_or_404
 from django.contrib.auth.decorators import login_required
-from .models import Board
-from .forms import BoardForm
+from django.views.decorators.http import require_POST
+from .models import Board, Comment
+from .forms import BoardForm, CommentForm
 
 # Create your views here.
 def index(request):
-    boards = Board.objects.order_by('-pk')
+    boards = get_list_or_404(Board.objects.order_by('-pk'))    # 가져올 리스트가 없으면 404페이지
     context = {
         'boards' : boards,
     }
@@ -35,8 +36,10 @@ def detail(request, board_pk):
     # board = Board.objects.get(pk=board_pk)
     # 객체 가져오는 방식 다르게 함, 없는 게시물 번호 url로 이동하면 404메세지
     board = get_object_or_404(Board, pk=board_pk)
+    comment_form = CommentForm()
     context = {
         'board' : board,
+        'comment_form': comment_form,
     }
     return render(request, 'boards/detail.html', context)
     
@@ -71,3 +74,24 @@ def update(request, board_pk):
             'board' : board,
     }
     return render(request, 'boards/forms.html', context)
+    
+    
+# 댓글 출력 및 입력
+@require_POST    # 애초에 POST방식만 들어오도록!
+@login_required
+def comment_create(request, board_pk):
+    form = CommentForm(request.POST)
+    if form.is_valid():   # 값 검증
+        comment = form.save(commit=False)
+        comment.user = request.user
+        comment.board_id = board_pk
+        comment.save()
+    return redirect('boards:detail', board_pk)
+
+# 아래 방식으로 하면 분기가 훨씬 깔끔해짐 - 분기할 필요 없음
+@require_POST
+@login_required
+def comment_delete(request, board_pk, comment_pk):
+    comment = get_object_or_404(Comment, pk=comment_pk)
+    comment.delete()
+    return redirect('boards:detail', board_pk)
